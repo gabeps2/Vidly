b238723b2a72cb7c72889db9edf677a9ad9b7d7f
Gabriel_Silva <gabriel.psilva226@gmail.com>
2020-09-21 10:29:22 -0300

    26 - Changing the Model

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public List<Customer> customersList = new List<Customer>
+        {
+            new Customer() { name="John Smith", id=1 },
+            new Customer() {name="Mary Williams", id = 2},
+        };
+
+
+        // GET: Customers
+        [Route("Customers")]
+        public ActionResult Customers()
+        {
+            var viewModel = new CustomersViewModel()
+            {
+                customerList = this.customersList,
+            };
+            return View(viewModel);
+        }
+
+        // GET: Customers/Details/5
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            foreach (var customer in customersList)
+            {
+                if (customer.id == id)
+                {
+                    return View(customer);
+                }
+            }
+            return View(new Customer() { id = -1 });
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        private List<Movie> moviesList = new List<Movie>
+        {
+            new Movie(){name="Shrek!",id=1},
+            new Movie(){name="Wall-e",id=2}
+        };
+        // GET: Movies
+        [Route("Movies")]
+        public ActionResult Movies()
+        {
+            var viewModel = new MoviesViewModel()
+            {
+                listMovies = this.moviesList,
+            };
+            return View(viewModel);
+        }
+
+        // GET: Movies/Details/5
+        [Route("Movies/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            foreach (var movie in moviesList)
+            {
+                if (movie.id == id)
+                {
+                    return View(movie);
+                }
+            }
+            return View(new Movie() { id = -1 });
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
